e20659df117ce887aadd7d5f56824d9465c6fbe8
Asraf407 <Asraf.uddin4407@gmail.com>
2022-09-29 00:36:52 +0600

    react toastify installed

--- src/Componants/Header/Header.js
+++ src/Componants/Header/Header.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Thid id header</h2>
-        </div>
-    );
-};
+      
+//         <div>
+//           import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// function CollapsibleExample() {
+//   return (
+    
+    <div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Proactive Acitivity Club</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {/* <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+            
+          </Nav>
+         
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+</div>
+  );
+}
+
 
 export default Header;
--- src/Componants/Home/Home.js
+++ src/Componants/Home/Home.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Home.css';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [cart , setCart] = useState([])
+
+    useEffect( () =>{
+        // fetch('../fakeData/products.json')
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+       
+    },[]);
+
+    const handleAddToCart = (product) =>{
+        console.log(product);
+        const newCart =[...cart,product];
+        setCart(newCart)
+    }
     return (
         <div>
-            <h3>This is Home</h3>
+          <h3 className="text-primary text-center ">Select Your Training</h3>
+          <div className="exercise-container">
+            <div className='excercise-container'>
+                <h2>four part</h2>
+            </div>
+            <div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, blanditiis. Ullam ea nulla ab incidunt delectus qui veniam, et repudiandae exercitationem, placeat omnis laudantium blanditiis. Expedita ullam repudiandae quidem adipisci!</p>
+              {/* <Cart cart ={cart}  ></Cart> */}
+            </div>
+            </div>
+          </div>
         </div>
     );
 };
