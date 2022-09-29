323e85835a17b3d136d1c621955344a304422246
Asraf407 <Asraf.uddin4407@gmail.com>
2022-09-29 23:57:21 +0600

    finalized  added

--- src/Componants/Header/Header.js
+++ src/Componants/Header/Header.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-      
-//         <div>
-//           import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
-// function CollapsibleExample() {
-//   return (
-    
-    <div>
+       <div>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Proactive Acitivity Club</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            {/* <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+        
             
           </Nav>
          
